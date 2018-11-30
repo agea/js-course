e7b546b30e2bbfd8d199933594cdb1a75e158bae
Andrea Agili <agea@drwolf.it>
2018-11-30 10:21:12 +0100

    :construction: loading commit info

--- repository.js
+++ repository.js
@@ -8,9 +8,34 @@
 
     function init() {
         document.getElementById('title').innerText = repo;
-        fetch(baseUrl + repo + '/commits').then(function (response) {
-            console.log(response);
-        });
+        loadCommits();
+    }
+
+    function loadCommits() {
+        toggleElement('loading', true);
+        return fetch(baseUrl + repo + '/commits')
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (commits) {
+                toggleElement('loading', false);
+                var commitsElement = document.getElementById('commits');
+                var html = '<ul>';
+
+                commits.forEach(function (commit) {
+                    html += '<li> <span class="sha">' + commit.sha.substring(0, 7) + '</span> - ' +
+                        commit.commit.message + '</li>';
+                });
+
+                html += '</ul>';
+                commitsElement.innerHTML = html;
+
+            });
+    }
+
+    function toggleElement(id, toggle) {
+        var element = document.getElementById(id);
+        element.style.display = toggle ? 'block' : 'none';
     }
 
 })();
