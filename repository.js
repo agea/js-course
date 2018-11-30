b3db1ab2d6bfdc17b4089f1ba3d68c51b3f81843
Andrea Agili <agea@drwolf.it>
2018-11-30 10:53:07 +0100

    :sparkles: pagination

--- repository.js
+++ repository.js
@@ -1,6 +1,7 @@
 (function () {
-    var repo = 'agea/js-course';
+    var repo = 'angular/angular';
     var baseUrl = 'https://api.github.com/repos/';
+    var nextUrl = null;
 
     document.addEventListener('DOMContentLoaded', function () {
         init();
@@ -9,17 +10,27 @@
     function init() {
         document.getElementById('title').innerText = repo;
         loadCommits();
+        document.getElementById('next').addEventListener('click', function () {
+            loadCommits(nextUrl);
+        });
     }
 
-    function loadCommits() {
+    function loadCommits(url) {
+        url = url || baseUrl + repo + '/commits';
         toggleElement('loading', true);
-        return fetch(baseUrl + repo + '/commits')
+        return fetch(url)
             .then(function (response) {
-                return response.json()
+
+                var link = response.headers.get('Link');
+                if (link.indexOf('rel="next"') != -1) {
+                    var url = link.split(';')[0];
+                    nextUrl = url.substring(1, url.length - 1);
+                }
+                console.log(link);
+                return response.json();
             })
             .then(function (commits) {
                 toggleElement('loading', false);
-                var commitsElement = document.getElementById('commits');
                 var html = '<ul>';
 
                 commits.forEach(function (commit) {
@@ -28,7 +39,7 @@
                 });
 
                 html += '</ul>';
-                commitsElement.innerHTML = html;
+                document.getElementById('commits').innerHTML = html;
 
             });
     }
