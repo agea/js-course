6f80cd605efafc55cdf24b0082649d2a8b17dc9b
Andrea Agili <agea@drwolf.it>
2018-11-30 12:25:55 +0100

    :lock: token, append commits

--- repository.js
+++ repository.js
@@ -2,6 +2,7 @@
     var repo = 'angular/angular';
     var baseUrl = 'https://api.github.com/repos/';
     var nextUrl = null;
+    var token = null;
 
     document.addEventListener('DOMContentLoaded', function () {
         init();
@@ -9,18 +10,30 @@
 
     function init() {
         document.getElementById('title').innerText = repo;
-        loadCommits();
+
+        loadToken().then(loadCommits);
+
         document.getElementById('next').addEventListener('click', function () {
             loadCommits(nextUrl);
         });
     }
 
+    function loadToken() {
+        return fetch('token.json')
+            .then(function (response) {
+                return response.json();
+            }).then(function (json) {
+                token = json;
+            });
+    }
+
     function loadCommits(url) {
         url = url || baseUrl + repo + '/commits';
         toggleElement('loading', true);
-        return fetch(url)
+        var headers = new Headers();
+        headers.append('Authorization', 'token ' + token);
+        return fetch(url, { headers: headers })
             .then(function (response) {
-
                 var link = response.headers.get('Link');
                 if (link.indexOf('rel="next"') != -1) {
                     var url = link.split(';')[0];
@@ -31,15 +44,13 @@
             })
             .then(function (commits) {
                 toggleElement('loading', false);
-                var html = '<ul>';
-
+                var html = '';
                 commits.forEach(function (commit) {
                     html += '<li> <span class="sha">' + commit.sha.substring(0, 7) + '</span> - ' +
                         commit.commit.message + '</li>';
                 });
-
-                html += '</ul>';
-                document.getElementById('commits').innerHTML = html;
+                html += '';
+                document.getElementById('commits').innerHTML += html;
 
             });
     }
